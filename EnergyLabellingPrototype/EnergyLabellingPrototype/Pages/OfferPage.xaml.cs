e2dfd8fe637c1f3bb0a5c05a87383272fbc5fa2b
Mjarlund <mikkel.jarlund@gmail.com>
2016-11-10 16:44:43 +0100

    Added global styling.

--- EnergyLabellingPrototype/EnergyLabellingPrototype/Pages/OfferPage.xaml.cs
+++ EnergyLabellingPrototype/EnergyLabellingPrototype/Pages/OfferPage.xaml.cs
@@ -23,11 +23,10 @@ namespace EnergyLabellingPrototype.Pages
         public OfferPage()
         {
             InitializeComponent();
-
-            //foreach(TabItem tab in tabControl.Items)
-            //{
-            //    tab.Content = OfferGrid("dataGrid" + tab.Name);
-            //}
+            if(dataGridComponents.Items.Count == 0)
+            {
+                tabControl.Visibility = Visibility.Collapsed;
+            }
         }
 
         private void back_Click(object sender, RoutedEventArgs e)
@@ -38,31 +37,6 @@ namespace EnergyLabellingPrototype.Pages
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
         }
-
-        //private DataGrid OfferGrid(string binding)
-        //{
-        //    var columnHeaderDictionary = new Dictionary<string, string>()
-        //        {
-        //            {"description", "Beskrivelse"},
-        //            {"amount", "Mængde"},
-        //            {"unitPrice", "Enhedspris"},
-        //            {"costPrice", "Kostpris"},
-        //            {"salesUnitPrice", "Salgs EP"},
-        //            {"salesPriceAbs", "Salgspris (Abs.)"},
-        //            {"totalGrossMargin", "Samlet DB"}
-        //        };
-
-        //    var dg = new DataGrid();
-        //    foreach (var entry in columnHeaderDictionary)
-        //    {
-        //        dg.Columns.Add(new DataGridTextColumn()
-        //        {
-        //            Header = entry.Value,
-        //            Binding = new Binding(entry.Key)
-        //        });
-        //    }
-
-        //    return dg;
-        //}
+        
     }
 }
