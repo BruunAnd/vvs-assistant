9fde9c1513d63484c01f23357c2d08bb65fc9474
Mysjkin <iarogolo@gmail.com>
2016-11-15 09:38:50 +0100

    no message

---
+++ iaro/structureExample/structureExample/CalculationManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structureExample
+{
+    class CalculationManager
+    {
+        ICalculation GetCalcFunc(List<ICalculateable> data)
+        {
+            ICalculation func = new HeatPumpCalculationStrategy();
+            HeapPumpStrategyData Heat = new HeapPumpStrategyData();
+            Heat.heatpumpDatAtt1 = 2;
+            func.Calculate<HeapPumpStrategyData>(Heat);
+            return func;
+        }
+    }
+}
