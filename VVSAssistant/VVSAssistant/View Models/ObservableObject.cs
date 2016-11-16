e2908e71d3b9c13cb1a85fa5238d7a9544776913
Mjarlund <mikkel.jarlund@gmail.com>
2016-11-16 11:59:30 +0100

    New .VS (Start of coding process)

---
+++ VVSAssistant/VVSAssistant/View Models/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VVSAssistant.View_Models
+{
+    /// <summary>
+    /// Base class for ViewModel classes.
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
