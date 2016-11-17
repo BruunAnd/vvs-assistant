05348b8e969e603e96de79bbcf1fbcc310dabfbd
andersjkbsn <andersjkbsn@gmail.com>
2016-11-17 10:56:21 +0100

    Added DataSheet to model

--- VVSAssistant/VVSAssistant/Models/AssistantModel1.Designer.cs
+++ VVSAssistant/VVSAssistant/Models/AssistantModel1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Documents\ds305e16\VVSAssistant\VVSAssistant\Models\AssistantModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Zen\Documents\ds305e16\VVSAssistant\VVSAssistant\Models\AssistantModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
