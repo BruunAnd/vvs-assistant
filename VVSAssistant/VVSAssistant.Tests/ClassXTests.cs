f80997571e64aeab1a8b1ae4e636b4aac02d7309
Mysjkin <iarogolo@gmail.com>
2016-11-18 16:57:32 +0100

    Unit test mappe

---
+++ VVSAssistant/VVSAssistant.Tests/ClassXTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace VVSAssistant.Tests
+{
+    [TestFixture]
+    public class ClassXTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [Category("Very Important Test Never Delete")]
+        //Method_StateUnderTest_ExpectedBehavior
+        public void IsAndersFaglord_IstUndFaglord_True()
+        {
+            //arrange
+            bool andersIstUndFaglord = false;
+            //act
+            andersIstUndFaglord = true;
+            //assert
+            Assert.IsTrue(andersIstUndFaglord);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
