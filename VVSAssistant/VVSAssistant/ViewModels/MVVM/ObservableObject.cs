aef805e8ea36e84db4c7b98aab11a93764202657
Mysjkin <iarogolo@gmail.com>
2016-11-18 15:30:05 +0100

    SetProperty i observable object der checker om to værdier er de samme før den kalder propertychanged.

--- VVSAssistant/VVSAssistant/ViewModels/MVVM/ObservableObject.cs
+++ VVSAssistant/VVSAssistant/ViewModels/MVVM/ObservableObject.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace VVSAssistant.ViewModels
@@ -10,9 +11,25 @@ namespace VVSAssistant.ViewModels
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void onPropertyChanged([CallerMemberName] string propertyName = null)
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        /// <summary>
+        /// Sets a property if the field parameter is different from the
+        /// value parmeter, returns true when a property is set and false
+        /// otherwise.
+        /// </summary>
+        protected bool SetProperty<T>(ref T field, T value,
+            [CallerMemberName] string propname = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propname);
+                return true;
+            }
+            return false;
         }
     }
 }
