fb999812f1c2cf54adea1e42c98771f918fc281a
Mysjkin <iarogolo@gmail.com>
2016-11-21 07:54:54 +0100

    ViewModelBase med validation logik, få med tests

---
+++ VVSAssistant/VVSAssistant.Tests/ViewModelTests/MVVMTests/ViewModelBaseTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using VVSAssistant.ViewModels.MVVM;
+using System.ComponentModel.DataAnnotations;
+
+namespace VVSAssistant.Tests.ViewModelTests.MVVMTests
+{
+    [TestFixture]
+    public class ViewModelBaseTests
+    {
+        [Test]
+        [TestCase(null, "Required")]
+        [TestCase("Value", null)]
+        public void ViewModelBaseIndexer_ValidateRequiredProperty(string value, string expectedError)
+        {
+            var vmstub = new ViewModelStub();
+            vmstub.RequiredPropertyTest = value;
+            Assert.AreEqual(vmstub["RequiredPropertyTest"], expectedError);
+        }
+        [Test]
+        [TestCase(0, "Range")]
+        [TestCase(11, "Range")]
+        [TestCase(5, null)]
+        public void ViewModelBaseIndexer_ValidateRequiredProperty(int value, string expectedError)
+        {
+            var vmstub = new ViewModelStub();
+            vmstub.RangeTest = value;
+            Assert.AreEqual(vmstub["RangeTest"], expectedError);
+        }
+    }
+
+    internal class ViewModelStub : ViewModelBase
+    {
+        [Required(ErrorMessage ="Required")]
+        public string RequiredPropertyTest { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Range(1,10, ErrorMessage="Range")]
+        public int RangeTest { get; set; }
+    }
+}
---
+++ VVSAssistant/VVSAssistant/ViewModels/MVVM/ViewModelBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVSAssistant.ViewModels.MVVM
+{
+    public class ViewModelBase : ObservableObject, IDataErrorInfo
+    {
+        public string this[string propName]
+        {
+            get
+            {
+                return ValidateProperty(propName);
+            }
+        }
+        /// <summary>
+        /// Validates a property based on its DataAnnotations
+        /// </summary>
+        /// <param name="Property Name"></param>
+        /// <returns> Error Message or null if the property data is valid </returns>
+        protected string ValidateProperty([CallerMemberName] string propName = null)
+        {
+            ValidationContext context = new ValidationContext(this, null, null);
+            context.MemberName = propName;
+            var errors = new List<ValidationResult>();
+            if (Validator.TryValidateObject(this, context, errors, true))
+                return null;
+            else
+            {
+                var validationError = errors.SingleOrDefault(error => error.MemberNames
+                                            .Any(member => member == propName));
+                return validationError?.ErrorMessage;
+            }
+        }
+
+        public string Error
+        {
+            get
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
