90b978ff66c09325349a8791da3aabd05fdb0102
Drunar <andreascml@gmail.com>
2016-11-21 13:52:37 +0100

    Added Properties to calculation results

--- VVSAssistant/VVSAssistant/Functions/Calculation/EEICalculationResult.cs
+++ VVSAssistant/VVSAssistant/Functions/Calculation/EEICalculationResult.cs
@@ -9,6 +9,20 @@ namespace VVSAssistant.Functions.Calculation
     // Calculation dat goes here
     public class EEICalculationResult
     {
-
+        public IEEICalculation CalculationType { get; set; }
+        public float PrimaryHeatingUnitAFUE { get; set; }
+        public float EffectOfTemperatureRegulatorClass { get; set; }
+        public float SecondaryBoilerAFUE { get; set; }
+        public float EffectOfSecondaryBoiler { get; set; }
+        public float SecondaryHeatPumpAFUE { get; set; }
+        public float EffectOfSecondaryHeatPump { get; set; }
+        public float SolarCollectorArea { get; set; }
+        public float ContainerVolume { get; set; }
+        public float SolarCollectorEffectiveness { get; set; }
+        public float ContainerClassification { get; set; }
+        public float PackagedSolutionAtNormalTemperaturesAFUE { get; set; }
+        public float PackagedSolutionAtColdTemperaturesAFUE { get; set; }
+        public float PackagedSolutionAtWarmTemperaturesAFUE { get; set; }
+        public float LowHeatAFUE { get; set; }
     }
 }
