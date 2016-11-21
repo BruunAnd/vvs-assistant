58a7ae3feb83e0a40888c8de7356f6dd731a2fc6
Drunar <andreascml@gmail.com>
2016-11-21 23:58:23 +0100

    Added half of calculations for primary heatpump calc

--- VVSAssistant/VVSAssistant/Functions/Calculation/WeightingBetweenPrimaryAndSecondaryChooser.cs
+++ VVSAssistant/VVSAssistant/Functions/Calculation/WeightingBetweenPrimaryAndSecondaryChooser.cs
@@ -9,46 +9,67 @@ namespace VVSAssistant.Functions.Calculation
 {
     public class WeightingBetweenPrimaryAndSecondaryChooser
     {
+        //below did not work
 
-        static public DataTable PrimHeatPumpSecBoilerGrid;
+        //public static Dictionary<float, float> PrimHeatPumpNoContainer = new Dictionary<float, float>()
+        //{
+        //    {0.0f, 1.0f }, {0.1f, 0.70f }, {0.2f, 0.45f }, {0.3f, 0.25f }, {0.4f, 0.15f }, {0.5f, 0.05f }, {0.6f, 0.02f }, {0.7f, 0.0f }
+        //};
+        //public static Dictionary<float, float> PrimHeatPumpWithContainer = new Dictionary<float, float>()
+        //{
+        //    {0.0f, 1.0f }, {0.1f, 0.63f }, {0.2f, 0.30f }, {0.3f, 0.15f }, {0.4f, 0.06f }, {0.5f, 0.02f }, {0.6f, 0.0f }, {0.7f, 0.0f }
+        //};
 
-        private void InitiateDataTables()
-        { 
-             DataTable _primHeatSecBoil = new DataTable();
-            _primHeatSecBoil.Columns.Add("Result");
-            _primHeatSecBoil.Columns.Add("II no Container");
-            _primHeatSecBoil.Columns.Add("II with Container");
-            
-            float[] row1 = { 0.0f, 1.0f, 1.0f};
-            float[] row2 = { 0.1f, 0.70f, 0.63f };
-            float[] row3 = { 0.2f, 0.45f, 0.30f };
-            float[] row4 = { 0.3f, 0.25f, 0.15f };
-            float[] row5 = { 0.4f, 0.15f, 0.06f };
-            float[] row6 = { 0.5f, 0.05f, 0.02f };
-            float[] row7 = { 0.6f, 0.02f, 0.0f };
-            float[] row8 = { 0.7f, 0.0f, 0.0f };
+        ////for later use with primary boiler.
+        //public static Dictionary<float, float> PrimBoilerNoContainer = new Dictionary<float, float>();
+        //public static Dictionary<float, float> PrimBoilerWithContainer = new Dictionary<float, float>();
 
-            _primHeatSecBoil.Rows.Add(row1);
-            _primHeatSecBoil.Rows.Add(row2);
-            _primHeatSecBoil.Rows.Add(row3);
-            _primHeatSecBoil.Rows.Add(row4);
-            _primHeatSecBoil.Rows.Add(row5);
-            _primHeatSecBoil.Rows.Add(row6);
-            _primHeatSecBoil.Rows.Add(row7);
-            _primHeatSecBoil.Rows.Add(row8);
+        public static float[] resultsPrimHeat = new float[] { 0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f };
+        public static float[] PrimHeatNoContainer = new float[] { 1.0f, 0.7f, 0.45f, 0.25f, 0.15f, 0.05f, 0.02f, 0.0f };
+        public static float[] PrimHeatWithContainer = new float[] { 1.0f, 0.63f, 0.30f, 0.15f, 0.06f, 0.02f, 0.00f, 0.0f };
 
-            PrimHeatPumpSecBoilerGrid = _primHeatSecBoil;
-        }
 
-        public float GetWeighting()
+        public float GetWeightingPrimHeat(float input, bool hasContainer)
         {
-            InitiateDataTables();
+            float[] Array;
 
-            DataRow[] ResultsRow = PrimHeatPumpSecBoilerGrid.Select("Result = 0.0");
+            if(hasContainer == true)
+            {
+                Array = PrimHeatWithContainer;
+            }
+            else
+            {
+                Array = PrimHeatNoContainer;
+            }
 
-            Console.WriteLine(ResultsRow[2]);
-            return 0.0f;
+            float output = 0.0f;
+            bool _outputFound = false;
+            int i = 0;
+
+            while (_outputFound == false)
+            {
+                if (input < resultsPrimHeat[i])
+                {
+                    i++;
+                }
+                else if(input == resultsPrimHeat[i])
+                {
+                    output = Array[i];
+                    _outputFound = true;
+                }
+                else
+                {
+                    output = LiniarInterpolation(resultsPrimHeat, Array, i, input);
+                    _outputFound = true;
+                }
+            }
+            return output;
         }
-       
+
+        private float LiniarInterpolation(float[] results, float[] IIvalues, int i, float input)
+        {
+            return IIvalues[i - 1] + (IIvalues[i] - IIvalues[i - 1]) / (results[i] - results[i - 1]) * (input - results[i - 1]);
+        }
+
     }
 }
