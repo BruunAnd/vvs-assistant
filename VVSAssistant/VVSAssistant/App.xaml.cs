0e679dd75767ec520a8bb2c06fe2d0b9164f92f2
andersjkbsn <andersjkbsn@gmail.com>
2016-11-21 13:44:53 +0100

    navigation stuff

--- VVSAssistant/VVSAssistant/App.xaml.cs
+++ VVSAssistant/VVSAssistant/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using VVSAssistant.Database;
 using VVSAssistant.Models;
 using VVSAssistant.Models.DataSheets;
+using VVSAssistant.ViewModels.MVVM;
 
 namespace VVSAssistant
 {
@@ -16,63 +17,17 @@ namespace VVSAssistant
     /// </summary>
     public partial class App : Application
     {
+        public static INavigationService Navigation;
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            new AssistantContext().Database.Delete();
-            // Test database stuff
-            using (var db = new AssistantContext())
-            {
-                // add a new client 'Iaro'
-                var clientInformation = new ClientInformation() { Name="Iaroslav", Address="Kvadratet", Email="iaro@russia.ru", PhoneNumber="88888888"};
-                db.ClientInformation.Add(clientInformation);
-                var client = new Client { CreationDate = DateTime.Now, ClientInformation = clientInformation };
-                db.Clients.Add(client);
-                db.SaveChanges();
-                // offer example
-                var offer = new Offer
-                {
-                    Client = client,
-                    CreationDate = DateTime.Now
-                };
-                var offerInformation = new OfferInformation()
-                {
-                    Description = "Example description",
-                    Price = 500
-                };
-                db.OfferInformation.Add(offerInformation);
-                offer.OfferInformation = offerInformation;
-                var packagedSolution = new PackagedSolution()
-                {
-                    CreationDate = DateTime.Now,
-                    Name = "Example Solution"
-                };
-                db.PackagedSolutions.Add(packagedSolution);
-                var appliance = new Appliance()
-                {
-                    CreationDate = DateTime.Now,
-                    Name = "Example Appliance",
-                    Type = ApplianceTypes.Boiler
-                };
-                packagedSolution.Appliances.Add(appliance);
-                var heatPumpDataSheet = new HeatingUnitDataSheet()
-                {
-                    Price = 200
-                };
-                db.DataSheets.Add(heatPumpDataSheet);
-                appliance.DataSheet = heatPumpDataSheet;
-                offer.PackagedSolution = packagedSolution;
-                db.Offers.Add(offer);
-                db.SaveChanges();
-            }
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
 
-            // get datasheet test
-            using (var ctx = new AssistantContext())
-            {
-                ctx.DataSheets.ToList().ForEach(sheet => Console.WriteLine(sheet is HeatingUnitDataSheet));
-            }
-
+            Navigation = new NavigationService(mainWindow.Frame);
+            Navigation.GoToNavigation();
         }
     }
 }
