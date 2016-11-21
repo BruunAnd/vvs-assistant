adb05b7cd55f475b169ad39deb264c33fbe2feeb
Drunar <andreascml@gmail.com>
2016-11-21 16:13:53 +0100

    Started working on HeatPumpAsPrimaryStrategy

--- VVSAssistant/VVSAssistant/Models/DataSheets/TemperatureControllerDataSheet.cs
+++ VVSAssistant/VVSAssistant/Models/DataSheets/TemperatureControllerDataSheet.cs
@@ -9,8 +9,8 @@ namespace VVSAssistant.Models.DataSheets
         [NotMapped]
         public static Dictionary<string, float> ClassBonus = new Dictionary<string, float>()
         {
-            {"1", 1.0f }, {"2", 2.0f }, {"3", 1.5f }, {"4", 2.0f }, {"5", 3.0f }, {"6", 4.0f }, {"7", 3.5f }, {"8", 5.0f }
+            {null, 0.0f }, {"1", 1.0f }, {"2", 2.0f }, {"3", 1.5f }, {"4", 2.0f }, {"5", 3.0f }, {"6", 4.0f }, {"7", 3.5f }, {"8", 5.0f }
         };
-        public int Class { get; set; }
+        public string Class { get; set; } 
     }
 }
