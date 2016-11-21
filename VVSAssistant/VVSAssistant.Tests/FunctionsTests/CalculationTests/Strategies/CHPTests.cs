9a6c5ff512d93746acf8d290c30dba95efdc736d
Mysjkin <iarogolo@gmail.com>
2016-11-21 09:39:10 +0100

    Calculation test mapper og classer

---
+++ VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/Strategies/CHPTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVSAssistant.Tests.FunctionsTests.CalculationTests.Strategies
+{
+    [TestFixtrue]
+    public class CHPStrategyTests
+    {
+    }
+}
