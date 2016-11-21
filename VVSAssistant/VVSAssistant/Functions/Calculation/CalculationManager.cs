a848af7fcbf63a90e5e428f112e4a92fc2a0351f
Mysjkin <iarogolo@gmail.com>
2016-11-21 13:41:32 +0100

    Appliance implementerer ICalculation og start på calculationManager

--- VVSAssistant/VVSAssistant/Functions/Calculation/CalculationManager.cs
+++ VVSAssistant/VVSAssistant/Functions/Calculation/CalculationManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VVSAssistant.Functions.Calculation.Interfaces;
+using VVSAssistant.Functions.Calculation.Strategies;
 using VVSAssistant.Models;
 
 namespace VVSAssistant.Functions.Calculation
@@ -12,7 +13,13 @@ namespace VVSAssistant.Functions.Calculation
     {
         public IEEICalculation SelectCalculationStreategy(PackagedSolution package)
         {
-            return null;
+            switch(package.PrimaryHeatingUnit.Type)
+            {
+                case ApplianceTypes.Heatpump:
+                    return new HeatPumpAsPrimary();
+                default:
+                    return null;
+            }
         } 
     }
 }
