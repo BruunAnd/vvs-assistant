e7569d5e8fd81ba270c12500169c0449f95491ce
Drunar <andreascml@gmail.com>
2016-11-21 10:42:11 +0100

    New DataSheets

---
+++ VVSAssistant/VVSAssistant/Models/DataSheets/SolarCollectorDataSheet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVSAssistant.Models.DataSheets
+{
+    class SolarCollectorDataSheet
+    {
+        public float area { get; set; }
+        public float efficency { get; set; }
+    }
+}
