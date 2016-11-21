e8f09fb883c7e864467e0b83c305d836452a4457
Mysjkin <iarogolo@gmail.com>
2016-11-21 14:49:50 +0100

    CalculationManager og appliancetypes starter på 1 i stedet for default(enum)

--- VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/CalculationManagerTests.cs
+++ VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/CalculationManagerTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,13 @@ using System.Threading.Tasks;
 
 namespace VVSAssistant.Tests.FunctionsTests.CalculationTests
 {
-    class CalculationManagerTests
+    [TestFixture]
+    public class CalculationManagerTests
     {
+        [Test]
+        public void SelectCalculationStrategy_SelectsHeatPumpStrategt_True()
+        {
+
+        }
     }
 }
--- VVSAssistant/VVSAssistant/Functions/Calculation/CalculationManager.cs
+++ VVSAssistant/VVSAssistant/Functions/Calculation/CalculationManager.cs
@@ -11,9 +11,39 @@ namespace VVSAssistant.Functions.Calculation
 {
     public class CalculationManager
     {
+        /// <summary>
+        /// Returns a Calculation strategy based on the primary type and 
+        /// packaged solution content.
+        /// </summary>
+        /// <param name="package"></param>
+        /// <returns>Calculation strategy</returns>
         public IEEICalculation SelectCalculationStreategy(PackagedSolution package)
         {
-            return null;
+            var primaryType = package.PrimaryHeatingUnit.Type;
+
+            switch (package.PrimaryHeatingUnit.Type)
+            {
+                case ApplianceTypes.Heatpump:
+                    return new HeatPumpAsPrimary();
+                case ApplianceTypes.Boiler:
+                    if (primaryType == ApplianceTypes.Boiler &&
+                        package.Appliances.Where(item =>
+                        item.Type != ApplianceTypes.SolarPanel) == null &&
+                        package.Appliances.SingleOrDefault(solarPanel =>
+                        solarPanel.Type == ApplianceTypes.SolarPanel) != null)
+                        return new BoilerForWater();
+                    else
+                        return new BoilerAsPrimary();
+                case ApplianceTypes.LowTempHeatPump:
+                    return new LowTempHeatPumpAsPrimary();
+                case default(ApplianceTypes):
+                    if (package.Appliances.Count > 0)
+                        return new CHPStrategy();
+                    else
+                        return null;
+                default:
+                    return null;
+            }
         } 
     }
 }
