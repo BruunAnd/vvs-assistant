98c6e619c8413edda19c3c714f1da517a9dd12e5
andersjkbsn <andersjkbsn@gmail.com>
2016-11-22 14:42:12 +0100

    compiles 4 me

--- VVSAssistant/VVSAssistant/Models/Appliance.cs
+++ VVSAssistant/VVSAssistant/Models/Appliance.cs
@@ -11,5 +11,6 @@ namespace VVSAssistant.Models
         public DateTime CreationDate { get; set; }
         public ApplianceTypes Type { get; set; }
         public virtual DataSheet DataSheet { get; set; }
+        public virtual ICollection<PackagedSolution> PackagedSolutions { get; set; }
     }
 }
