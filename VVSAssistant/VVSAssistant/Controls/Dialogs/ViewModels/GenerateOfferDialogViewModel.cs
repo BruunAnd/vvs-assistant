5db11100f555a06719ccbe956c2850fd8062504e
Anders Brams <anders@brams.dk>
2016-11-22 16:40:40 +0100

    Saving Client info and Offer title, intro and outro from GenerateOfferDialog

--- VVSAssistant/VVSAssistant/Controls/Dialogs/ViewModels/GenerateOfferDialogViewModel.cs
+++ VVSAssistant/VVSAssistant/Controls/Dialogs/ViewModels/GenerateOfferDialogViewModel.cs
@@ -14,7 +14,74 @@ namespace VVSAssistant.Controls.Dialogs.ViewModels
         public ObservableCollection<ClientViewModel> Clients;
         public RelayCommand CloseCommand { get; }
         public RelayCommand SaveCommand { get; }
-        private bool canSave = false;
+
+        public string ClientName
+        {
+            get { return Offer.Client.ClientInformation.Name; }
+            set
+            {
+                Offer.Client.ClientInformation.Name = value;
+                OnPropertyChanged();
+                OnPropertyChanged("OfferIntro");
+            }
+        }
+
+        public string ClientAddress
+        {
+            get { return Offer.Client.ClientInformation.Address; }
+            set
+            {
+                Offer.Client.ClientInformation.Address = value;
+                OnPropertyChanged();
+                OnPropertyChanged("OfferIntro");
+            }
+        }
+
+        public string ClientEmail
+        {
+            get { return Offer.Client.ClientInformation.Email; }
+            set
+            {
+                Offer.Client.ClientInformation.Email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string ClientPhone
+        {
+            get { return Offer.Client.ClientInformation.PhoneNumber; }
+            set
+            {
+                Offer.Client.ClientInformation.PhoneNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OfferIntro
+        {
+            get { return Offer.OfferInformation.Intro; }
+            set
+            {
+                Offer.OfferInformation.Intro = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OfferOutro
+        {
+            get { return Offer.OfferInformation.Outro; }
+            set
+            {
+                Offer.OfferInformation.Outro = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OfferTitle
+        {
+            get { return Offer.OfferInformation.Title; }
+            set { Offer.OfferInformation.Title = value; OnPropertyChanged(); }
+        }
 
         public GenerateOfferDialogViewModel(OfferViewModel offer, ObservableCollection<ClientViewModel> clients, IDialogCoordinator dialogCoordinator, Action<GenerateOfferDialogViewModel> closeHandler)
         {
