9bebee584a0bee8ecf86a352b7d6d110fdec2710
Mysjkin <iarogolo@gmail.com>
2016-11-22 14:15:35 +0100

    BoilerAsPrimary is done, ready to tests

--- VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/Strategies/BoilerForWaterAsPrimaryTests.cs
+++ VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/Strategies/BoilerForWaterAsPrimaryTests.cs
@@ -10,6 +10,7 @@ namespace VVSAssistant.Tests.FunctionsTests.CalculationTests.Strategies
     using VVSAssistant.Functions.Calculation.Strategies;
     using Models.DataSheets;
     using Models;
+    using Functions.Calculation;
 
     [TestFixture]
     public class BoilerForWaterAsPrimaryTests
@@ -18,6 +19,22 @@ namespace VVSAssistant.Tests.FunctionsTests.CalculationTests.Strategies
         public void CalculateEEI_CalculatesTheCorrectResult_FirstOffer()
         {
             
+        }
+        [Test]
+        [TestCase(15, 2, 48)]
+        [TestCase(10,5, 64)]
+        [TestCase(35, 10, 158)]
+        public void SolCalMethodQaux(int pumpConsumption, int standbyConsumption, float expected)
+        {
+            var package = new PackagedSolution() { PrimaryHeatingUnit = new Appliance() { DataSheet = new WaterHeatingUnitDataSheet()} };
+            var data = package.PrimaryHeatingUnit.DataSheet as WaterHeatingUnitDataSheet;
+            data.SolPumpConsumption = pumpConsumption;
+            data.SolStandbyConsumption = standbyConsumption;
+
+            var calculation = new BoilerForWater();
+            var result = calculation.SolCalMethodQaux(package.PrimaryHeatingUnit.DataSheet as WaterHeatingUnitDataSheet);
+
+            Assert.AreEqual(result, expected);
         }
     }
 }
