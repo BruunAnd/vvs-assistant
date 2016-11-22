c4eba05bc3e3ef102f9410c3f7d87a00b8e9b411
Anders Brams <anders@brams.dk>
2016-11-22 14:34:55 +0100

    Further preparation for dialog in GenerateOffer

--- VVSAssistant/VVSAssistant/Controls/Dialogs/ViewModels/GenerateOfferDialogViewModel.cs
+++ VVSAssistant/VVSAssistant/Controls/Dialogs/ViewModels/GenerateOfferDialogViewModel.cs
@@ -3,16 +3,36 @@ using System.Threading;
 using VVSAssistant.ViewModels.MVVM;
 using MahApps.Metro.Controls.Dialogs;
 using VVSAssistant.ViewModels;
+using System.ComponentModel;
 
 namespace VVSAssistant.Controls.Dialogs.ViewModels
 {
     class GenerateOfferDialogViewModel : ViewModelBase
     {
-        private OfferViewModel _offer;
+        public OfferViewModel Offer;
+        public RelayCommand CloseCommand { get; }
+        public RelayCommand SaveCommand { get; }
+        private bool canSave = false;
 
         public GenerateOfferDialogViewModel(OfferViewModel offer, IDialogCoordinator dialogCoordinator, Action<GenerateOfferDialogViewModel> closeHandler)
         {
-            _offer = offer;
+            Offer = offer;
+
+            SaveCommand = new RelayCommand(x =>
+            {
+                ConfirmSaveDialog(closeHandler);
+            }, x => true);
+
+
+            CloseCommand = new RelayCommand(x =>
+            {
+                closeHandler(this);
+            });
+        }
+
+        private void ConfirmSaveDialog(Action<GenerateOfferDialogViewModel> closeHandler)
+        {
+            closeHandler(this);
         }
     }
 }
