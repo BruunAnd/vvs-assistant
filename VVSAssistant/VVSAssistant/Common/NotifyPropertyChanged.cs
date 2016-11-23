72cb07d5142a384bd69979af6ba602bc02a44c74
Mjarlund <mikkel.jarlund@gmail.com>
2016-11-23 14:31:16 +0100

    Model changes

---
+++ VVSAssistant/VVSAssistant/Common/NotifyPropertyChanged.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace VVSAssistant.Common
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        /// <summary>
+        /// Sets a property if the field parameter is different from the
+        /// value parmeter, returns true when a property is set and false
+        /// otherwise.
+        /// </summary>
+        protected bool SetProperty<T>(ref T field, T value,
+            [CallerMemberName] string propname = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(propname);
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- VVSAssistant/VVSAssistant/Models/UnitPrice.cs
+++ VVSAssistant/VVSAssistant/Models/UnitPrice.cs
@@ -1,13 +1,35 @@
 ﻿using System;
+using VVSAssistant.Common;
 
 namespace VVSAssistant.Models
 {
-    public class UnitPrice
+    public class UnitPrice : NotifyPropertyChanged
     {
         public int Id { get; set; }
-        public int Quantity { get; set; }
-        public double UnitCostPrice { get; set; }
-        public double UnitSalesPrice { get; set; }
+
+        private int _quantity;
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { SetProperty<int>(ref _quantity, value); }
+        }
+        
+        // Update CostPrice if the UnitCostPrice is changed.
+        private double _unitCostPrice;
+        public double UnitCostPrice
+        {
+            get { return _unitCostPrice; }
+            set { SetProperty<double>(ref _unitCostPrice, value); OnPropertyChanged("CostPrice"); }
+        }
+
+        // Update SalesPrice if the UnitSalesPrice is changed.
+        private double _unitSalesPrice;
+        public double UnitSalesPrice
+        {
+            get { return _unitSalesPrice; }
+            set { SetProperty<double>(ref _unitSalesPrice, value); OnPropertyChanged("SalesPrice"); }
+        }
+        
 
         public double SalesPrice => Math.Abs(UnitSalesPrice * Quantity);
         public double CostPrice => UnitCostPrice * Quantity;
