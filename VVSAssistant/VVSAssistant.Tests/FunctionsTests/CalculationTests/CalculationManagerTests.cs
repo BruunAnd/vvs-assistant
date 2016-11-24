4001dedbc5cde9075da70055cca220e09b6515ea
Drunar <andreascml@gmail.com>
2016-11-24 20:29:40 +0100

    CalculationsManagerReadyForTesting

--- VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/CalculationManagerTests.cs
+++ VVSAssistant/VVSAssistant.Tests/FunctionsTests/CalculationTests/CalculationManagerTests.cs
@@ -14,34 +14,34 @@ namespace VVSAssistant.Tests.FunctionsTests.CalculationTests
     [TestFixture]
     public class CalculationManagerTests
     {
-        [Test]
-        public void SelectCalculationStrategy_ReturnsAEEICalculationType_True()
-        {
-            var package = new PackagedSolution() { PrimaryHeatingUnit = new Appliance() { Type = ApplianceTypes.Boiler } };
-            var calcManager = new CalculationManager();
+        //[Test]
+        //public void SelectCalculationStrategy_ReturnsAEEICalculationType_True()
+        //{
+        //    var package = new PackagedSolution() { PrimaryHeatingUnit = new Appliance() { Type = ApplianceTypes.Boiler } };
+        //    var calcManager = new CalculationManager();
 
-            Assert.IsTrue(typeof(IEEICalculation).IsAssignableFrom(calcManager.SelectCalculationStreategies(package).GetType()));
-        }
+        //    Assert.IsTrue(typeof(IEEICalculation).IsAssignableFrom(calcManager.SelectCalculationStreategies(package).GetType()));
+        //}
 
-        // Integration Test between Appliance and CalculationSelector
-        [Test]
-        [TestCase(ApplianceTypes.Boiler, null, typeof(BoilerAsPrimary))]
-        [TestCase(ApplianceTypes.HeatPump, null, typeof(HeatPumpAsPrimary))]
-        [TestCase(ApplianceTypes.LowTempHeatPump, null, typeof(HeatPumpAsPrimary))]
-        [TestCase(ApplianceTypes.Boiler, ApplianceTypes.SolarPanel, typeof(BoilerForWater))]
-        public void SelectCalculationStrategy_SelectsCorrectStrategy(ApplianceTypes type,
-            ApplianceTypes secondApplianceType, Type EEICalculation)
-        {
-            // Arrange
-            var package = new PackagedSolution();
-            var calcManager = new CalculationManager();
+        //// Integration Test between Appliance and CalculationSelector
+        //[Test]
+        //[TestCase(ApplianceTypes.Boiler, null, typeof(BoilerAsPrimary))]
+        //[TestCase(ApplianceTypes.HeatPump, null, typeof(HeatPumpAsPrimary))]
+        //[TestCase(ApplianceTypes.LowTempHeatPump, null, typeof(HeatPumpAsPrimary))]
+        //[TestCase(ApplianceTypes.Boiler, ApplianceTypes.SolarPanel, typeof(BoilerForWater))]
+        //public void SelectCalculationStrategy_SelectsCorrectStrategy(ApplianceTypes type,
+        //    ApplianceTypes secondApplianceType, Type EEICalculation)
+        //{
+        //    // Arrange
+        //    var package = new PackagedSolution();
+        //    var calcManager = new CalculationManager();
 
-            //Act
-            package.PrimaryHeatingUnit = new Appliance() { Type = type };
-            package.Appliances.Add(new Appliance() { Type = secondApplianceType});
+        //    //Act
+        //    package.PrimaryHeatingUnit = new Appliance() { Type = type };
+        //    package.Appliances.Add(new Appliance() { Type = secondApplianceType});
 
-            //Assert
-            Assert.AreEqual(calcManager.SelectCalculationStreategies(package).GetType(), EEICalculation);
-        }
+        //    //Assert
+        //    Assert.AreEqual(calcManager.SelectCalculationStreategies(package).GetType(), EEICalculation);
+        //}
     }
 }
