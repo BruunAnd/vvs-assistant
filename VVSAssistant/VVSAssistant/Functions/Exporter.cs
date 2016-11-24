ebc9d5a89c8671926473c321f7771ae6edcf0847
Mjarlund <mikkel.jarlund@gmail.com>
2016-11-24 17:55:51 +0100

    Added import and export database

--- VVSAssistant/VVSAssistant/Functions/Exporter.cs
+++ VVSAssistant/VVSAssistant/Functions/Exporter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VVSAssistant.Database;
 using VVSAssistant.Models;
 
 namespace VVSAssistant.Functions
@@ -18,5 +19,6 @@ namespace VVSAssistant.Functions
         {
 
         }
+        
     }
 }
