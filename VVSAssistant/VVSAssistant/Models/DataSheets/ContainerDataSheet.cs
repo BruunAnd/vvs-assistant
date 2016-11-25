6a4892193b332c24366c7a3023a7466461789a14
andersjkbsn <andersjkbsn@gmail.com>
2016-11-25 12:21:18 +0100

    Added extended wpf and propertygrid when editing an appliance

--- VVSAssistant/VVSAssistant/Models/DataSheets/ContainerDataSheet.cs
+++ VVSAssistant/VVSAssistant/Models/DataSheets/ContainerDataSheet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace VVSAssistant.Models.DataSheets
 {
+    [DisplayName(@"Beholder, datablad")]
     class ContainerDataSheet : DataSheet
     {
         [NotMapped]
@@ -14,8 +16,12 @@ namespace VVSAssistant.Models.DataSheets
         {
             {"A*", 0.95f }, {"A", 0.91f }, {"B", 0.86f}, {"C", 0.83f}, {"D", 0.81f}, {"E", 0.81f}, {"F", 0.81f}, {"G", 0.81f}
         };
+
+        [DisplayName(@"Volumen")]
         public float Volume { get; set; }
+        [DisplayName(@"Klassificering")]
         public string Classification { get; set; }
+        [DisplayName(@"Stående tab (lel)")]
         public float StandingLoss { get; set; }
     }
 }
