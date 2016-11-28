e6fb0d86181f3db3c0270597e7fce82452d91f9d
Anders Brams <anders@brams.dk>
2016-11-28 14:11:01 +0100

    Initial model in Final

---
+++ Final/Final/Model/Offer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Model
+{
+    class Offer
+    {
+        public virtual PackagedSolution PackagedSolution { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual OfferInformation OfferInformation { get; set; }
+    }
+}
