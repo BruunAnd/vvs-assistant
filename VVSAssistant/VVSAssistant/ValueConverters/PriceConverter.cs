96ee1bd27a5ec29f02488533d91eaa84d6877686
andersjkbsn <andersjkbsn@gmail.com>
2016-11-29 12:08:03 +0100

    added price value converter

---
+++ VVSAssistant/VVSAssistant/ValueConverters/PriceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VVSAssistant.ValueConverters
+{
+    public class PriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is double)) return value;
+            return $"kr. {((double) value).ToString("n2", new CultureInfo("da-DK"))}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
