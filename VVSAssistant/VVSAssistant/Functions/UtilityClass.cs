99521b691e409f63e18a9f25a80bac6089cc1d33
Mjarlund <mikkel.jarlund@gmail.com>
2016-12-06 12:27:45 +0100

    Cleaned up initializers

--- VVSAssistant/VVSAssistant/Functions/UtilityClass.cs
+++ VVSAssistant/VVSAssistant/Functions/UtilityClass.cs
@@ -6,11 +6,11 @@ namespace VVSAssistant.Functions
     public static class UtilityClass
     {
 
-        public static float[] ResultsPrimHeat = new float[] { 0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f };
-        public static float[] PrimHeatNoContainer = new float[] { 1.0f, 0.7f, 0.45f, 0.25f, 0.15f, 0.05f, 0.02f, 0.0f };
-        public static float[] PrimHeatWithContainer = new float[] { 1.0f, 0.63f, 0.30f, 0.15f, 0.06f, 0.02f, 0.0f, 0.0f };
-        public static float[] PrimBoilNoContainer = new float[] { 0.30f, 0.35f, 0.55f, 0.75f, 0.85f, 0.95f, 0.98f, 1.0f };
-        public static float[] PrimBoilWithContainer = new float[] { 0.0f, 0.37f, 0.70f, 0.85f, 0.94f, 0.98f, 1.0f, 1.0f };
+        public static float[] ResultsPrimHeat = { 0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f };
+        public static float[] PrimHeatNoContainer = { 1.0f, 0.7f, 0.45f, 0.25f, 0.15f, 0.05f, 0.02f, 0.0f };
+        public static float[] PrimHeatWithContainer = { 1.0f, 0.63f, 0.30f, 0.15f, 0.06f, 0.02f, 0.0f, 0.0f };
+        public static float[] PrimBoilNoContainer = { 0.30f, 0.35f, 0.55f, 0.75f, 0.85f, 0.95f, 0.98f, 1.0f };
+        public static float[] PrimBoilWithContainer = { 0.0f, 0.37f, 0.70f, 0.85f, 0.94f, 0.98f, 1.0f, 1.0f };
 
 
         public static float GetWeighting(float input, bool hasContainer, bool primIsHeatPump)
