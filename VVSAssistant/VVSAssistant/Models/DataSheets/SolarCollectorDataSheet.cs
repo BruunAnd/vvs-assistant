daec73d8d5513f108646dff9de18db84db63ba1b
Anders <andersjkbsn@gmail.com>
2016-12-12 18:57:35 +0100

    no message

--- VVSAssistant/VVSAssistant/Models/DataSheets/SolarCollectorDataSheet.cs
+++ VVSAssistant/VVSAssistant/Models/DataSheets/SolarCollectorDataSheet.cs
@@ -26,36 +26,23 @@ namespace VVSAssistant.Models.DataSheets
         // Incidence Angle Modifier 
         [DisplayName(@"Indfaldsvinkel korrektionsfaktor")]
         public float IAM { get; set; }
-        [Browsable(false)]
-        public bool IsRoomHeater { get; set; }
-        [Browsable(false)]
-        public bool IsWaterHeater { get; set; }
 
         public override string ToString()
         {
-            var room = "";
-            var water = "";
-            var N0String = "";
+            var n0String = "";
             var a1String = "";
             var a2String = "";
-            var IAMString = "";
-            if (IsRoomHeater)
-                room = ", rumopvarmning";
-            if (IsWaterHeater)
-                water = ", vandopvarmning";
+            var iamString = "";
             if (Math.Abs(N0) > 0)
-                N0String = $", Effektivitet ved nulbelastning: {N0}";
+                n0String = $", Effektivitet ved nulbelastning: {N0}";
             if (Math.Abs(a1) > 0)
                 a1String = $", 1. Ordens koefficient: {a1} W/(m\u00b2 K)";
             if (Math.Abs(a2) > 0)
                 a2String = $", 2. Ordens koefficient: {a2}";
             if (Math.Abs(IAM) > 0)
-                IAMString = $", Indfaldsvinkel korrektionsfaktor: {IAM}";
+                iamString = $", Indfaldsvinkel korrektionsfaktor: {IAM}";
 
-
-
-
-            return $"Areal: {Area}m\u00b2, Energieffektivitet: {Efficency}%{room}{water}{N0String}{a1String}{a2String}{IAMString}";
+            return $"Areal: {Area}m\u00b2, Energieffektivitet: {Efficency}%{n0String}{a1String}{a2String}{iamString}";
         }
     }
 }
