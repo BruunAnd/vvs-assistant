f91d9adc1c61654a2452c9227ca2ab5a4629bc2f
Mysjkin <iarogolo@gmail.com>
2016-12-13 11:19:08 +0100

    fjernede makecopy fra applaince da den var ubruglig anders b.

--- VVSAssistant/VVSAssistant/Models/Appliance.cs
+++ VVSAssistant/VVSAssistant/Models/Appliance.cs
@@ -7,7 +7,7 @@ using VVSAssistant.ValueConverters;
 
 namespace VVSAssistant.Models
 {
-    public class Appliance : ICalculable, ICopyable, IEquatable<Appliance>
+    public class Appliance : ICalculable, IEquatable<Appliance>
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -33,21 +33,6 @@ namespace VVSAssistant.Models
         public override string ToString()
         {
             return $"{Name} ({new ApplianceTypeConverter().Convert(Type, null, null, null)})";
-        }
-
-        public object MakeCopy()
-        {
-            var copy = Activator.CreateInstance(this.GetType()); //New object of same type
-            var properties = GetType().GetProperties(); //All properties from this object
-            foreach (var pi in copy.GetType().GetProperties()) //For all the properties in the new object
-            {
-                var matchingProperty = properties.First(p => p.Name == pi.Name); //Find the property with the same name in this object
-                if (pi == this.GetType().GetProperty("Description")) // Description does not have a set method
-                    continue;
-                else
-                    pi.SetValue(copy, matchingProperty.GetValue(this)); //Set the new object's property with this name to the value of the same property in this object
-            }
-            return copy;
         }
 
         public bool Equals(Appliance other)
